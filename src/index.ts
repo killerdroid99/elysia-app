42b186fe5cffc83d6beca16163c6bb74af861c5a
Aniket Jena <ajempire09@gmail.com>
2024-03-06 12:11:54 +0530

    Init app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(
+  cors({
+    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.get("/", (ctx) => {
+  return { msg: "hello world" };
+});
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
